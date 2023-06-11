fb3b10388885c0af5ef31797117046200f1d2ea8
Matthew Gabriel <74555083+Choochera@users.noreply.github.com>
2023-06-11 09:07:09 -0500

    Update serviceUtils.ts

--- src/utils/serviceUtils.ts
+++ src/utils/serviceUtils.ts
@@ -2,7 +2,6 @@ import CONSTANTS from "../resources/ResourceConstants";
 
 export function buildHeadersWithBasicAuth(): { Authorization: string, 'Content-Type': string } {
     return {
-        Authorization: 'Basic dXNlcm5hbWU6cGFzc3dvcmQ=',
         'Content-Type': CONSTANTS.GLOBAL.JSON
     }
 }
