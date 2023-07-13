e6689430dd8a321dabe0224881adaba7710606ea
Choochera <gabrielmatthew20@gmail.com>
2023-07-12 19:39:00 -0500

    Update to match rename

--- src/utils/serviceUtils.ts
+++ src/utils/serviceUtils.ts
@@ -2,7 +2,7 @@ import CONSTANTS from "../resources/ResourceConstants";
 
 export function buildHeadersWithBasicAuth(): { Authorization: string, 'Content-Type': string } {
     return {
-        Authorization: '',
+        Authorization: 'Basic dXNlcm5hbWU6cGFzc3dvcmQ=',
         'Content-Type': CONSTANTS.GLOBAL.JSON
     }
 }
