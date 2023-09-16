79a6ac63e44de0d29b9b17c63e2f2ef68b83fe4c
Choochera <gabrielmatthew20@gmail.com>
2023-09-16 14:26:17 -0500

    Remove getBulkidentitiesAndOptionalDiscounts + identity service and typings

--- src/resources/entities/identity/IdentitiesAndDiscounts.ts
+++
@@ -1,9 +0,0 @@
-import { SimpleDiscount } from "@/resources/services/discount/discount.typings";
-import Identity from "./Identity";
-
-export default interface IdentitiesAndDiscounts {
-
-    discounts?: SimpleDiscount[],
-    identities: Identity
-     
-}
