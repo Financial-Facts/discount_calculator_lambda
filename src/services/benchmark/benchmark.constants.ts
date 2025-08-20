b72815dbb824e6fe1478a05df64ad7ad150caf89
Matthew Gabriel <gabrielmatthew20@gmail.com>
2025-08-20 08:48:21 -0500

    Update benchmark ratio mapping logic

--- src/services/benchmark/benchmark.constants.ts
+++ src/services/benchmark/benchmark.constants.ts
@@ -26,3 +26,533 @@ export const industryNameMap: Record<string, string> = {
     'Paper, Lumber & Forest Products': 'Building Materials'
 }
 
+export const benchmarkIndustryMapping = [
+    {
+        "Industry": "Advertising Agencies",
+        "Average P/S ratio": "1.1"
+    },
+    {
+        "Industry": "Aerospace &amp; Defense",
+        "Average P/S ratio": "2.63"
+    },
+    {
+        "Industry": "Agricultural Inputs",
+        "Average P/S ratio": "1.19"
+    },
+    {
+        "Industry": "Airlines",
+        "Average P/S ratio": "0.48"
+    },
+    {
+        "Industry": "Airports &amp; Air Services",
+        "Average P/S ratio": "1.51"
+    },
+    {
+        "Industry": "Aluminum",
+        "Average P/S ratio": "0.53"
+    },
+    {
+        "Industry": "Apparel Manufacturing",
+        "Average P/S ratio": "0.83"
+    },
+    {
+        "Industry": "Apparel Retail",
+        "Average P/S ratio": "0.72"
+    },
+    {
+        "Industry": "Asset Management",
+        "Average P/S ratio": "3.3"
+    },
+    {
+        "Industry": "Auto Manufacturers",
+        "Average P/S ratio": "1.16"
+    },
+    {
+        "Industry": "Auto Parts",
+        "Average P/S ratio": "0.81"
+    },
+    {
+        "Industry": "Auto &amp; Truck Dealerships",
+        "Average P/S ratio": "1.09"
+    },
+    {
+        "Industry": "Banks - Diversified",
+        "Average P/S ratio": "3.76"
+    },
+    {
+        "Industry": "Banks - Regional",
+        "Average P/S ratio": "3.4"
+    },
+    {
+        "Industry": "Beverages - Non-Alcoholic",
+        "Average P/S ratio": "2.09"
+    },
+    {
+        "Industry": "Beverages - Wineries &amp; Distilleries",
+        "Average P/S ratio": "1.61"
+    },
+    {
+        "Industry": "Biotechnology",
+        "Average P/S ratio": "6.46"
+    },
+    {
+        "Industry": "Broadcasting",
+        "Average P/S ratio": "0.79"
+    },
+    {
+        "Industry": "Building Materials",
+        "Average P/S ratio": "3.05"
+    },
+    {
+        "Industry": "Building Products &amp; Equipment",
+        "Average P/S ratio": "2.3"
+    },
+    {
+        "Industry": "Business Equipment &amp; Supplies",
+        "Average P/S ratio": "0.82"
+    },
+    {
+        "Industry": "Capital Markets",
+        "Average P/S ratio": "2.61"
+    },
+    {
+        "Industry": "Chemicals",
+        "Average P/S ratio": "0.69"
+    },
+    {
+        "Industry": "Coking Coal",
+        "Average P/S ratio": "1.35"
+    },
+    {
+        "Industry": "Communication Equipment",
+        "Average P/S ratio": "2.38"
+    },
+    {
+        "Industry": "Computer Hardware",
+        "Average P/S ratio": "1.93"
+    },
+    {
+        "Industry": "Conglomerates",
+        "Average P/S ratio": "1.31"
+    },
+    {
+        "Industry": "Consulting Services",
+        "Average P/S ratio": "1.83"
+    },
+    {
+        "Industry": "Consumer Electronics",
+        "Average P/S ratio": "2.69"
+    },
+    {
+        "Industry": "Credit Services",
+        "Average P/S ratio": "2.22"
+    },
+    {
+        "Industry": "Diagnostics &amp; Research",
+        "Average P/S ratio": "4.06"
+    },
+    {
+        "Industry": "Discount Stores",
+        "Average P/S ratio": "1.21"
+    },
+    {
+        "Industry": "Drug Manufacturers - General",
+        "Average P/S ratio": "4.11"
+    },
+    {
+        "Industry": "Drug Manufacturers - Specialty &amp; Generic",
+        "Average P/S ratio": "2.98"
+    },
+    {
+        "Industry": "Education &amp; Training Services",
+        "Average P/S ratio": "1.97"
+    },
+    {
+        "Industry": "Electrical Equipment &amp; Parts",
+        "Average P/S ratio": "2.01"
+    },
+    {
+        "Industry": "Electronic Components",
+        "Average P/S ratio": "3.14"
+    },
+    {
+        "Industry": "Electronic Gaming &amp; Multimedia",
+        "Average P/S ratio": "2.25"
+    },
+    {
+        "Industry": "Electronics &amp; Computer Distribution",
+        "Average P/S ratio": "0.57"
+    },
+    {
+        "Industry": "Engineering &amp; Construction",
+        "Average P/S ratio": "1.58"
+    },
+    {
+        "Industry": "Entertainment",
+        "Average P/S ratio": "1.38"
+    },
+    {
+        "Industry": "Farm &amp; Heavy Construction Machinery",
+        "Average P/S ratio": "1.06"
+    },
+    {
+        "Industry": "Farm Products",
+        "Average P/S ratio": "0.84"
+    },
+    {
+        "Industry": "Financial Data &amp; Stock Exchanges",
+        "Average P/S ratio": "4.97"
+    },
+    {
+        "Industry": "Food Distribution",
+        "Average P/S ratio": "0.32"
+    },
+    {
+        "Industry": "Footwear &amp; Accessories",
+        "Average P/S ratio": "1.14"
+    },
+    {
+        "Industry": "Furnishings, Fixtures &amp; Appliances",
+        "Average P/S ratio": "0.65"
+    },
+    {
+        "Industry": "Gambling",
+        "Average P/S ratio": "2.15"
+    },
+    {
+        "Industry": "Gold",
+        "Average P/S ratio": "3.39"
+    },
+    {
+        "Industry": "Grocery Stores",
+        "Average P/S ratio": "0.5"
+    },
+    {
+        "Industry": "Healthcare Plans",
+        "Average P/S ratio": "0.39"
+    },
+    {
+        "Industry": "Health Information Services",
+        "Average P/S ratio": "3.53"
+    },
+    {
+        "Industry": "Home Improvement Retail",
+        "Average P/S ratio": "1.26"
+    },
+    {
+        "Industry": "Household &amp; Personal Products",
+        "Average P/S ratio": "2.03"
+    },
+    {
+        "Industry": "Industrial Distribution",
+        "Average P/S ratio": "1.77"
+    },
+    {
+        "Industry": "Information Technology Services",
+        "Average P/S ratio": "2.04"
+    },
+    {
+        "Industry": "Insurance Brokers",
+        "Average P/S ratio": "2.96"
+    },
+    {
+        "Industry": "Insurance - Diversified",
+        "Average P/S ratio": "2.09"
+    },
+    {
+        "Industry": "Insurance - Life",
+        "Average P/S ratio": "1.33"
+    },
+    {
+        "Industry": "Insurance - Property &amp; Casualty",
+        "Average P/S ratio": "1.65"
+    },
+    {
+        "Industry": "Insurance - Reinsurance",
+        "Average P/S ratio": "0.82"
+    },
+    {
+        "Industry": "Insurance - Specialty",
+        "Average P/S ratio": "2.48"
+    },
+    {
+        "Industry": "Integrated Freight &amp; Logistics",
+        "Average P/S ratio": "1.15"
+    },
+    {
+        "Industry": "Internet Content &amp; Information",
+        "Average P/S ratio": "2.74"
+    },
+    {
+        "Industry": "Internet Retail",
+        "Average P/S ratio": "1.3"
+    },
+    {
+        "Industry": "Leisure",
+        "Average P/S ratio": "1.36"
+    },
+    {
+        "Industry": "Lodging",
+        "Average P/S ratio": "1.71"
+    },
+    {
+        "Industry": "Luxury Goods",
+        "Average P/S ratio": "0.9"
+    },
+    {
+        "Industry": "Marine Shipping",
+        "Average P/S ratio": "1.34"
+    },
+    {
+        "Industry": "Medical Care Facilities",
+        "Average P/S ratio": "1.12"
+    },
+    {
+        "Industry": "Medical Devices",
+        "Average P/S ratio": "3.74"
+    },
+    {
+        "Industry": "Medical Distribution",
+        "Average P/S ratio": "0.27"
+    },
+    {
+        "Industry": "Medical Instruments &amp; Supplies",
+        "Average P/S ratio": "4.22"
+    },
+    {
+        "Industry": "Metal Fabrication",
+        "Average P/S ratio": "1.95"
+    },
+    {
+        "Industry": "Mortgage Finance",
+        "Average P/S ratio": "1.73"
+    },
+    {
+        "Industry": "Oil &amp; Gas Drilling",
+        "Average P/S ratio": "0.69"
+    },
+    {
+        "Industry": "Oil &amp; Gas E&amp;P",
+        "Average P/S ratio": "1.65"
+    },
+    {
+        "Industry": "Oil &amp; Gas Equipment &amp; Services",
+        "Average P/S ratio": "1.16"
+    },
+    {
+        "Industry": "Oil &amp; Gas Integrated",
+        "Average P/S ratio": "1.3"
+    },
+    {
+        "Industry": "Oil &amp; Gas Midstream",
+        "Average P/S ratio": "1.85"
+    },
+    {
+        "Industry": "Oil &amp; Gas Refining &amp; Marketing",
+        "Average P/S ratio": "0.64"
+    },
+    {
+        "Industry": "Other Industrial Metals &amp; Mining",
+        "Average P/S ratio": "1.62"
+    },
+    {
+        "Industry": "Other Precious Metals &amp; Mining",
+        "Average P/S ratio": "3.95"
+    },
+    {
+        "Industry": "Packaged Foods",
+        "Average P/S ratio": "1.11"
+    },
+    {
+        "Industry": "Packaging &amp; Containers",
+        "Average P/S ratio": "0.97"
+    },
+    {
+        "Industry": "Paper &amp; Paper Products",
+        "Average P/S ratio": "0.25"
+    },
+    {
+        "Industry": "Personal Services",
+        "Average P/S ratio": "1.68"
+    },
+    {
+        "Industry": "Pollution &amp; Treatment Controls",
+        "Average P/S ratio": "3.39"
+    },
+    {
+        "Industry": "Publishing",
+        "Average P/S ratio": "0.99"
+    },
+    {
+        "Industry": "Railroads",
+        "Average P/S ratio": "2.63"
+    },
+    {
+        "Industry": "Real Estate - Development",
+        "Average P/S ratio": "3.22"
+    },
+    {
+        "Industry": "Real Estate - Diversified",
+        "Average P/S ratio": "3.94"
+    },
+    {
+        "Industry": "Real Estate Services",
+        "Average P/S ratio": "2.53"
+    },
+    {
+        "Industry": "Recreational Vehicles",
+        "Average P/S ratio": "0.82"
+    },
+    {
+        "Industry": "REIT - Diversified",
+        "Average P/S ratio": "4.48"
+    },
+    {
+        "Industry": "REIT - Healthcare Facilities",
+        "Average P/S ratio": "5.37"
+    },
+    {
+        "Industry": "REIT - Hotel &amp; Motel",
+        "Average P/S ratio": "1.13"
+    },
+    {
+        "Industry": "REIT - Industrial",
+        "Average P/S ratio": "7.55"
+    },
+    {
+        "Industry": "REIT - Mortgage",
+        "Average P/S ratio": "4.99"
+    },
+    {
+        "Industry": "REIT - Office",
+        "Average P/S ratio": "2.63"
+    },
+    {
+        "Industry": "REIT - Residential",
+        "Average P/S ratio": "5.74"
+    },
+    {
+        "Industry": "REIT - Retail",
+        "Average P/S ratio": "6.82"
+    },
+    {
+        "Industry": "REIT - Specialty",
+        "Average P/S ratio": "5.67"
+    },
+    {
+        "Industry": "Rental &amp; Leasing Services",
+        "Average P/S ratio": "2.06"
+    },
+    {
+        "Industry": "Residential Construction",
+        "Average P/S ratio": "1.21"
+    },
+    {
+        "Industry": "Resorts &amp; Casinos",
+        "Average P/S ratio": "1.26"
+    },
+    {
+        "Industry": "Restaurants",
+        "Average P/S ratio": "1.14"
+    },
+    {
+        "Industry": "Scientific &amp; Technical Instruments",
+        "Average P/S ratio": "4.18"
+    },
+    {
+        "Industry": "Security &amp; Protection Services",
+        "Average P/S ratio": "2.06"
+    },
+    {
+        "Industry": "Semiconductor Equipment &amp; Materials",
+        "Average P/S ratio": "4.14"
+    },
+    {
+        "Industry": "Semiconductors",
+        "Average P/S ratio": "4.51"
+    },
+    {
+        "Industry": "Software - Application",
+        "Average P/S ratio": "4.17"
+    },
+    {
+        "Industry": "Software - Infrastructure",
+        "Average P/S ratio": "3.96"
+    },
+    {
+        "Industry": "Solar",
+        "Average P/S ratio": "1.55"
+    },
+    {
+        "Industry": "Specialty Business Services",
+        "Average P/S ratio": "1.81"
+    },
+    {
+        "Industry": "Specialty Chemicals",
+        "Average P/S ratio": "1.97"
+    },
+    {
+        "Industry": "Specialty Industrial Machinery",
+        "Average P/S ratio": "3.08"
+    },
+    {
+        "Industry": "Specialty Retail",
+        "Average P/S ratio": "1.07"
+    },
+    {
+        "Industry": "Staffing &amp; Employment Services",
+        "Average P/S ratio": "1.15"
+    },
+    {
+        "Industry": "Steel",
+        "Average P/S ratio": "1.07"
+    },
+    {
+        "Industry": "Telecom Services",
+        "Average P/S ratio": "1.43"
+    },
+    {
+        "Industry": "Thermal Coal",
+        "Average P/S ratio": "1.09"
+    },
+    {
+        "Industry": "Tobacco",
+        "Average P/S ratio": "1.89"
+    },
+    {
+        "Industry": "Tools &amp; Accessories",
+        "Average P/S ratio": "2.27"
+    },
+    {
+        "Industry": "Travel Services",
+        "Average P/S ratio": "1.15"
+    },
+    {
+        "Industry": "Trucking",
+        "Average P/S ratio": "1.28"
+    },
+    {
+        "Industry": "Utilities - Diversified",
+        "Average P/S ratio": "1.96"
+    },
+    {
+        "Industry": "Utilities - Regulated Electric",
+        "Average P/S ratio": "2.78"
+    },
+    {
+        "Industry": "Utilities - Regulated Gas",
+        "Average P/S ratio": "1.91"
+    },
+    {
+        "Industry": "Utilities - Regulated Water",
+        "Average P/S ratio": "4.06"
+    },
+    {
+        "Industry": "Utilities - Renewable",
+        "Average P/S ratio": "3.04"
+    },
+    {
+        "Industry": "Waste Management",
+        "Average P/S ratio": "2.47"
+    }
+];
--- src/services/benchmark/benchmark.service.ts
+++ src/services/benchmark/benchmark.service.ts
@@ -1,23 +1,16 @@
-import HttpException from '@/utils/exceptions/HttpException';
-import { JSDOM } from 'jsdom';
-import ScrapeDataException from '@/utils/exceptions/ScrapeDataException';
 import { calculatorService } from '../../bootstrap';
 import { BenchmarkRatioPriceInput, BenchmarkRatioPrice } from './benchmark.typings';
 import { BenchmarkRatioPriceQuarterlyData } from '@/resources/discount-manager/discount-manager.typings';
 import { reduceTTM, getLastPeriodValue } from '@/utils/processing.utils';
-import { industryNameMap } from './benchmark.constants';
+import { benchmarkIndustryMapping, industryNameMap } from './benchmark.constants';
 import { benchmarkRatioPriceFunction } from '../calculator/calculator.service';
 
 class BenchmarkService {
 
     benchmarkSourceUrl: string;
-    benchmarkIndustryMapping: Record<string, number>;
-    isReady: Promise<void>;
 
     constructor(benchmarkSourceUrl: string) {
         this.benchmarkSourceUrl = benchmarkSourceUrl;
-        this.benchmarkIndustryMapping = {};
-        this.isReady = this.loadBenchmarkIndustryMap();
     }
 
     public getBenchmarkRatioPrice(cik: string, input: BenchmarkRatioPriceInput): BenchmarkRatioPrice {
@@ -55,91 +48,32 @@ class BenchmarkService {
     }
 
     private async fetchBenchmarkPsRatio(industry: string): Promise<number> {
-        return this.isReady.then(() => {
-            if (industry in this.benchmarkIndustryMapping) {
-                console.log(`Industry '${industry}' exists in benchmark map...`);
-                return this.benchmarkIndustryMapping[industry];
-            }
+        let mapping = benchmarkIndustryMapping.find(obj => obj.Industry === industry);
+        if (!!mapping) {
+            console.log(`Industry '${industry}' exists in benchmark map...`);
+            return +mapping['Average P/S ratio'];
+        }
 
-            const removedDash = ('' + industry).replaceAll(' - ', ' ');
-            if (removedDash in this.benchmarkIndustryMapping) {
-                console.log(`Industry '${removedDash}' exists in benchmark map...`);
-                return this.benchmarkIndustryMapping[removedDash];
-            }
+        const removedDash = ('' + industry).replaceAll(' - ', ' ');
+        mapping = benchmarkIndustryMapping.find(obj => obj.Industry === removedDash);
+        if (!!mapping) {
+            console.log(`Industry '${industry}' exists in benchmark map...`);
+            return +mapping['Average P/S ratio'];
+        }
 
-            if (industry in industryNameMap && industryNameMap[industry] in this.benchmarkIndustryMapping) {
+        if (industry in industryNameMap) {
+            
+            mapping = benchmarkIndustryMapping.find(obj => obj.Industry === industryNameMap[industry]);
+
+            if (!!mapping) {
                 console.log(`Industry '${industry}' exists in industry name map...`);
-                return this.benchmarkIndustryMapping[industryNameMap[industry]];
+                return +mapping['Average P/S ratio'];
             }
-            console.log(`Industry '${industry}' does not exist in benchmark map, returning default...`);
-            return 1.69;
-        });
-    }
-
-    private async loadBenchmarkIndustryMap(): Promise<void> {
-        return this.fetchPage()
-            .then(async HTML => {
-                return this.fetchFromWebOrCache(HTML)
-                    .then(document => {
-                        this.extractData(document);
-                    });
-            }).finally(() => {
-                console.log('Benchmark industry map has been loaded!');
-            });
-    }
-
-    private async fetchPage(): Promise<string> {
-        try {
-            return fetch(
-                this.benchmarkSourceUrl,
-                {
-                    headers: {
-                        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36',
-                        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-                        'Accept-Encoding': 'gzip, deflate, br, zstd'
-                    }
-                }
-            ).then(async (response: Response) => {
-                if (response.status !== 200) {
-                    const text = await response.text();
-                    throw new HttpException(response.status,
-                        `Error occurred while fetching benchmark page: ${text}`);
-                }
-                return response.text();
-            }).then((body: string) => {
-                return body;
-            });
-        } catch (err: any) {
-            throw new HttpException(err.status,
-                `Error occurred while fetching benchmark page: ${err.message}`)
         }
-    }
 
-    private async fetchFromWebOrCache(HTML: string): Promise<Document> {
-        const dom = new JSDOM(HTML);
-        return dom.window.document;
+        console.log(`Industry '${industry}' does not exist in benchmark map, returning default...`);
+        return 1.69;
     }
-
-    private extractData(document: Document): void {
-        const benchmarkTable: HTMLTableElement | null
-            = document.querySelector('#market-analysis-page > table:nth-child(12)');
-        if (!benchmarkTable) {
-            throw new ScrapeDataException('Industry benchmark table not found');
-        }
-        const tableData = Array.from(benchmarkTable.children[0].children).slice(1);
-        tableData.forEach(tr => {
-            const industry = this.cleanIndustryText(tr.children[0].innerHTML);
-            const benchmarkRatio = +tr.children[1].innerHTML;
-            this.benchmarkIndustryMapping[industry] = benchmarkRatio;
-        })
-    }
-
-    private cleanIndustryText(industry: string): string {
-        return industry
-            .replaceAll('&amp;', '&')
-            .replaceAll('—', '-');
-    }
-
 }
 
 export default BenchmarkService;
